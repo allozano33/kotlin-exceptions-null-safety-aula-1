9396cec579091d24bd26535395068d6bee892283
Alessandro Lozano <alessandro.lozano@mercadolivre.com>
2022-08-25 18:44:28 -0300

    adicionando-classes

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,27 +1,48 @@
-package br.com.alura.bytebank
-
-import br.com.alura.bytebank.modelo.Endereco
-
 fun main() {
     println("início main")
-    10/0
+
+    val entrada: String = "1.0"
+
+    val valorRecebido: Double? = try {
+        entrada.toDouble()
+    } catch (e: NumberFormatException) {
+        println("Problema na conversão")
+        e.printStackTrace()
+        null
+    }
+
+    val valorComTaxa: Double? = if (valorRecebido != null) {
+        valorRecebido + 0.1
+    } else {
+        null
+    }
+
+    if (valorComTaxa != null) {
+        println("valor recebido: $valorComTaxa")
+    } else {
+        println("valor inválido")
+    }
+
     funcao1()
     println("fim main")
 }
-
-fun funcao1(){
+fun funcao1() {
     println("início funcao1")
-    funcao2()
+    try {
+        funcao2()
+    } catch (e: ArithmeticException) {
+        e.printStackTrace()
+        println("ArithmeticException foi pegada")
+    }
     println("fim funcao1")
 }
-
 fun funcao2() {
     println("início funcao2")
-    for (i in 1..5){
-
+    for (i in 1..5) {
         println(i)
         val endereco = Any()
-        endereco as Endereco
+        throw SaldoInsuficienteException()
     }
     println("fim funcao2")
 }
+class SaldoInsuficienteException : Throwable("O Saldo é insuficiente")
